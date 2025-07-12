076133a292a81d5a27a430742d3f9a377cdaff0f
nisha <nisharenish14@gmail.com>
2025-07-12 18:34:37 +0530

    Removed duplicate nested folder and fixed project structure

--- TaskManagementApi/TaskManagementApp.Api/TaskManagementApp.Api/Models/TaskCategory.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TaskManagementApp.Api.Models
-{
-    public class TaskCategory
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- TaskManagementApi/TaskManagementApp.Api/TaskManagementApp.Api/Models/TaskPriority.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TaskManagementApp.Api.Models
-{
-    public class TaskPriority
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
