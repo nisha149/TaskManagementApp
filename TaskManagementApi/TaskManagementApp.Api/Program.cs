9d3987b503736c28daaa84d2948b3e80e5c3a852
nisha <nisharenish14@gmail.com>
2025-08-28 18:25:41 +0530

    Update: frontend and Backend changes

--- TaskManagementApi/TaskManagementApp.Api/Program.cs
+++ TaskManagementApi/TaskManagementApp.Api/Program.cs
@@ -89,7 +89,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "https://smart-manager.netlify.app")
+        policy.WithOrigins("https://localhost:5173", "https://smart-manager.netlify.app")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
